75f2bf85131619786a8aca83c8161cda24ceee50
Javad Shoja <javadshoja1999@gmail.com>
2025-07-10 13:02:17 +0330

    fix: update table component styles to include font and border adjustments for improved visual consistency

--- src/components/ui/table.tsx
+++ src/components/ui/table.tsx
@@ -20,7 +20,7 @@ function TableHeader({ className, ...props }: React.ComponentProps<'thead'>) {
   return (
     <thead
       data-slot='table-header'
-      className={cn('[&_tr]:border-b', className)}
+      className={cn('font-vazirmatn [&_tr]:border-b', className)}
       {...props}
     />
   )
@@ -30,7 +30,7 @@ function TableBody({ className, ...props }: React.ComponentProps<'tbody'>) {
   return (
     <tbody
       data-slot='table-body'
-      className={cn('[&_tr:last-child]:border-0', className)}
+      className={cn('font-vazirmatn [&_tr:last-child]:border-0', className)}
       {...props}
     />
   )
@@ -54,7 +54,7 @@ function TableRow({ className, ...props }: React.ComponentProps<'tr'>) {
     <tr
       data-slot='table-row'
       className={cn(
-        'hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors',
+        'font-vazirmatn hover:bg-muted/50 data-[state=selected]:bg-muted border-b transition-colors',
         className
       )}
       {...props}
